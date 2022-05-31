b93520b332a3671ab883d6deddb181004f2d8522
斉藤翔馬 <s.shoma415@icloud.com>
2022-05-31 22:44:13 +0900

    add container widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+            color: Colors.white,
+            child: Text('hello'),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
