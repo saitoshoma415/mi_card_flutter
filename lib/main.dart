da159397db11cd93676e11d6258b4878dad5dd8e
斉藤翔馬 <s.shoma415@icloud.com>
2022-06-18 04:33:58 +0900

    material icon

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,50 @@ class Myapp extends StatelessWidget {
                   fontSize: 20.0,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.grey,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+81 123 456 789',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'ConcertOne',
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.grey,
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.email_outlined),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'shoma@email.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontFamily: 'ConcertOne'),
+                  )
+                ],
+              ),
             )
           ],
         )),
